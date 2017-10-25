2fef99631d7ed7a768f212014a08bc4cfc26c090
joysa <joselin.solis.3b@gmail.com>
2017-10-25 20:36:15 -0300

    Ejercicio terminado

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -4,22 +4,30 @@ class SalesController < ApplicationController
   end
 
   def create
-  	sale = sales_params
-  	prepare_sale(sale)
-  	
+  	sale = Sale.new(sales_params)
+  	prepare_sale(sale) 
+    redirect_to sales_done_path 	
+  end
+
+  def done
+    @sale = Sale.all.last
   end
 
 private
   def sales_params
-  	params.require(:sale).permit.(:detail, :category, :value, :discount, :tax)
+  	params.require(:sale)
+    .permit(:cod, :detail, :category, :value, :discount, :tax)
   end
 
-  def prepare_sales
+  def prepare_sale(sale)
   	sale.cod = Random.rand(10...10000)
   	sale.value = (sales_params[:value].to_i)*(100 - sales_params[:discount].to_i)
-  	if sales_params[:tax] == 0 
-	  	sale.total = sale.value *1,19  
+  	if sales_params[:tax].to_i == 0 
+	  	sale.total = sale.value * 1.19
+      sale.tax = 19
+
 	  else
+      sale.tax = 0
 	  	sale.total = sale.value
 	  end
 	  	sale.save
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,9 +1,9 @@
 class Sale < ApplicationRecord
- validates :cod, uniqueness: :true
- validates :detail, presence: :true
- validates :category, inclusion: %w(uno dos tres cuatro cinco)
- validates_numericality_of :value, greater_than_or_equal_to: 0, only_integer: :true
+ validates :cod, uniqueness: true
+ validates :detail, presence: true
+ validates :category, inclusion: { in: [1,2,3,4,5]}
+ validates_numericality_of :value, greater_than_or_equal_to: 0, only_integer: true
  validates_numericality_of :discount, greater_than_or_equal_to: 40, 
  							greater_than_or_equal_to: 0, 
- 							only_integer: :true
+ 							only_integer: true
 end
